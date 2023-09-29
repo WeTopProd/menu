6e50b7b15071f1aa72e1331ff31ad678362c1f11
94R1K <dfdfdf.dfdf.1985@mail.ru>
2023-09-29 12:37:10 +0300

    deploy

--- backend/menu/urls.py
+++ backend/menu/urls.py
@@ -14,28 +14,28 @@ urlpatterns = [
 ]
 
 schema_view = get_schema_view(
-   openapi.Info(
-      title="Menu API",
-      default_version='v1',
-      description="Документация для API к меню",
-      license=openapi.License(name="MIT License"),
-   ),
-   public=True,
-   permission_classes=(permissions.AllowAny,),
+    openapi.Info(
+        title="Menu API",
+        default_version='v1',
+        description="Документация для API к меню",
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns += [
-   url(r'^swagger(?P<format>\.json|\.yaml)$',
-       schema_view.without_ui(cache_timeout=0), name='schema-json'),
-   url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
-       name='schema-swagger-ui'),
-   url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
-       name='schema-redoc'),
+    url(r'^swagger(?P<format>\.json|\.yaml)$',
+        schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0),
+        name='schema-swagger-ui'),
+    url(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'),
 ]
 
 if settings.DEBUG:
     urlpatterns = (
-        urlpatterns
-        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+            urlpatterns
+            + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+            + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     )
